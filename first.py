e88cd28f77dc0083bbf3eae4f39497c575158923
raunak-k-jha <raunakjhawork2024@gmail.com>
2026-09-13 07:51:31 +0530

    My_third_commit

--- first.py
+++ first.py
@@ -6,11 +6,19 @@ def fxadd(n1,n2,n3): # LOGIN_CODE.
 def fxsubs(n1,n2): # LOGOUT_CODE
     return n1-n2
 
-def fxmultiply(n1*n2*n3):
+def fxmultiply(n1,n2,n3):
     return n1*n2*n3
 
 def fxdivide(n1,n2):
     return n1/n2
 
+def fx(name):
+    print(name)
+
+def fx1():
+    print("Hello Good Morning!!")
+
 print(fxadd(1,2,3))
 print(fxsubs(6,5))
+fx("Raunak")
+fx1()
